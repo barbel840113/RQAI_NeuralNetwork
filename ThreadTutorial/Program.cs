7f57a04f4e0a5045a3ac796f3501f708281bf102
Michal Sivak <sivakmichal@gmail.com>
2018-09-09 00:52:52 +1000

    Trainigfds method

--- ThreadTutorial/Program.cs
+++ ThreadTutorial/Program.cs
@@ -1,26 +1,38 @@
 ﻿using System;
 using System.Threading;
+using System.Diagnostics;
 
 namespace ThreadTutorial
 {
     class Program
     {
         public static object readOnly = new object();
+        public static int readOnlyInt = 0;
+        public static Stopwatch stopWatch = new Stopwatch();
+        public static int intePointer = 0;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            for(int i =0; i< 100; i++)
+            stopWatch.Start();
+            for (intePointer = 0; intePointer < 100; intePointer++)
             {
-                lock(readOnly)
-                {
-                    new Thread(() => {
-                        Console.Write(i + " ");
-                    }).Start();
-                }
-               
+
+                new Thread(Startthread).Start();
             }
 
+            
             Console.ReadLine();
+        }
+           
+
+        public static void Startthread()
+        {
+            lock (readOnly)
+            {
+                Console.Write(intePointer + " ");
+            }
+            
         }
     }
 }
