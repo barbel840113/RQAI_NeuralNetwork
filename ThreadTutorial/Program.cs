d32b3f8f5fa27148fb8f4786658552288a3686d6
Michal Sivak <sivakmichal@gmail.com>
2018-09-08 23:23:33 +1000

    Add

---
+++ ThreadTutorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTutorial
+{
+    class Program
+    {
+        public static object readOnly = new object();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for(int i =0; i< 100; i++)
+            {
+                lock(readOnly)
+                {
+                    new Thread(() => {
+                        Console.Write(i + " ");
+                    }).Start();
+                }
+               
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
